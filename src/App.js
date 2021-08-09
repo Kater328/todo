ceeeef5f755b9a2f515eee10bb153acac8382407
kater328 <kattyzz328@gmail.com>
2021-08-09 13:51:54 +0300

    feat: implement header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import './App.css';
-import React from 'react';
+import "./App.css";
+import React from "react";
+import Header from "./Components/Header";
 
-function App() {
-  return (
-    <div></div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todos: [],
+    };
+  }
+
+  createTodo = (title) => {
+    this.setState({
+      todos: [
+        ...this.state.todos,
+        {id: Date.now(), title, completed: false}
+      ]
+    });
+  }
+
+  render() {
+    return (
+      <section className="todoapp">
+        <Header createTodo={this.createTodo} />
+      </section>
+    );
+  }
 }
 
 export default App;
